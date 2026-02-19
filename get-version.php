f68a6b851fe3b5960fadaba681a0f196bee0d757
Daniel Sturm <d.sturm@zirkeldesign.de>
2026-02-19 11:05:54 +0100

    ✨ Add version number to distribution archive filename

---
+++ get-version.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Extract version from plugin file for build scripts
+ */
+
+$content = file_get_contents(__DIR__ . '/regex-validation-for-gravity-forms.php');
+if (preg_match('/Version:\s*([0-9.]+)/', $content, $matches)) {
+    echo $matches[1];
+} else {
+    echo '0.0.0';
+}
